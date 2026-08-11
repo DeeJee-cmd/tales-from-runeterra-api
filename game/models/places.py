1184435fbce45de51ea0a7c2a7fc8c2e18d2fca4
Pasha Liashenko <zedxi236@ukr.net>
2026-08-11 17:04:44 +0300

    Update Region model fields

--- game/models/places.py
+++ game/models/places.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Region(models.Model):
-    name = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="regions/", null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
