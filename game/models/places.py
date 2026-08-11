6a7d500b08bc88f9dccc1d9af9c438b78e961e48
Pasha Liashenko <zedxi236@ukr.net>
2026-08-12 00:44:19 +0300

    Delete field `id` in model

--- game/models/places.py
+++ game/models/places.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Region(models.Model):
-    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to="regions/", null=True, blank=True)
