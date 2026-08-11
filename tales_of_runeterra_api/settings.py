e7ff306407ccabf971aa7a2f1bde250920c99a42
Pasha Liashenko <zedxi236@ukr.net>
2026-08-12 00:52:51 +0300

    Connected drf-spectacular & drf-spectacular-sidecar

--- tales_of_runeterra_api/settings.py
+++ tales_of_runeterra_api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
     "game",
 ]
 
@@ -121,3 +123,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "tales_of_runeterra_api",
+    "DESCRIPTION": "API backend for a tactical D&D-inspired RPG set in the LOL & LOR universe.",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
+}
